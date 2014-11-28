8e5bdaf95efa694951c2db68e22a37e2eaea06f8
Nasrul Aziz <nasrul.zf@gmail.com>
2014-11-29 05:41:02 +0700

    merubah kondisi setelah transaksi
    menghindari fungsi yang tidak diperlukan

--- src/sistempeminjaman/db/Koneksi.java
+++ src/sistempeminjaman/db/Koneksi.java
@@ -12,6 +12,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -28,6 +29,12 @@ public class Koneksi {
                 DriverManager.registerDriver(new Driver());
                 conn = DriverManager.getConnection("jdbc:mysql://localhost/dbs_pinjaman", "root", "");
             } catch (SQLException ex) {
+                System.out.println("Error : " + ex.getSQLState());
+                
+                if(ex.getSQLState().equalsIgnoreCase("08S01")){
+                    JOptionPane.showMessageDialog(null, "Periksa kembali pengaturan database anda, \nsilahkan periksa manualbook untuk panduan");
+                }
+                
                 Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
